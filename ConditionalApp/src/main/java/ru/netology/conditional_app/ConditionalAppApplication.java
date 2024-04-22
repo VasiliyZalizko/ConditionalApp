f9829fb0befb42ccf39a940e06d837ef3cb9e237
Vasiliy Zalizko <vasiliy.zalizko@gmail.com>
2024-04-22 22:42:34 +0300

    first commit

---
+++ ConditionalApp/src/main/java/ru/netology/conditional_app/ConditionalAppApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.conditional_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConditionalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConditionalAppApplication.class, args);
+	}
+
+}
---
+++ ConditionalApp/src/main/java/ru/netology/conditional_app/SystemProfile.java
@@ -0,0 +1,5 @@
+package ru.netology.conditional_app;
+
+public interface SystemProfile {
+    public String getProfile();
+}
